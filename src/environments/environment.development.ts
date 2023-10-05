cb411f14af514140f64b3902ee445a11458f5a17
Razgrizsx <razgrisss41@gmail.com>
2023-10-05 18:53:07 -0300

    api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: 'https://fakestoreapi.com/products',
-    stripeUrl: "http://localhost:3000/checkout",
+    stripeUrl: "https://web-store-angular.vercel.app/checkout",
     stripeKey: "pk_test_51Ls9QPAABAdrkD1f4GxIbKrVLEEsM39Ji7ilv1pGWVCizw2xsC8S3pQFmRqp2mchGSduhLiMCDdzMr1j8Od2bSzu00xnqfduAS"
 };
